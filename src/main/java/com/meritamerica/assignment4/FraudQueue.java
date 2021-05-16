9062d834ec7168c8fa4c0d3cf72dcffd091cf859
wolex_king <59701150+wolexcode@users.noreply.github.com>
2021-05-16 16:37:53 -0400

    current modify

--- src/main/java/com/meritamerica/assignment4/FraudQueue.java
+++ src/main/java/com/meritamerica/assignment4/FraudQueue.java
@@ -1,16 +1,27 @@
 package com.meritamerica.assignment4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FraudQueue {
+	List<Transaction> que;
+	
+	
 	
 	FraudQueue() {
+		this.que = new ArrayList<Transaction>();
+		
 		
 	}
 	
 	public void addTransaction(Transaction transaction) {
-		
+		que.add(transaction);
+			
 	}
 	public Transaction getTransaction() {
-		return null;
+		Transaction transaction = que.get(0);
+		que.remove(transaction);
+		return transaction;
 		
 	}
 	
